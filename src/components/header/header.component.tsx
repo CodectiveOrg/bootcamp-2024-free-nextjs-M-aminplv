89ff6602c500afa3be7bf1dc1478ae739154d778
Amin <pahlavanmohammadamin@gmail.com>
2025-01-01 09:40:04 +0330

    [ADD] not found page [FIXED] extra nav in header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -9,14 +9,6 @@ import style from "./header.module.css";
 
 function HeaderComponent() {
   const pathname = usePathname();
-  const url = [
-    { id: 1, href: "صفحه-1", txt: "صفحه1" },
-    { id: 2, href: "صفحه-2", txt: "صفحه2" },
-    { id: 3, href: "صفحه-3", txt: "صفحه3" },
-    { id: 4, href: "صفحه-4", txt: "صفحه4" },
-    { id: 5, href: "5", txt: "صفحه5" },
-    { id: 6, href: "صفحه-6", txt: "صفحه6" },
-  ];
   return (
     <header className={style.header}>
       <nav>
@@ -34,13 +26,6 @@ function HeaderComponent() {
               جستجو
             </Link>
           </li>
-          {url.map((item) => {
-            return (
-              <li key={item.id}>
-                <Link href={item.href}>{item.txt}</Link>
-              </li>
-            );
-          })}
         </ul>
       </nav>
       <button className={style.cta}>ورود|ثبت نام</button>
